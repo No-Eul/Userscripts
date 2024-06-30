bca8114be2a9e15cf603b479965f7f47737565f0
NoEul <67686234+no-eul@users.noreply.github.com>
2024-07-01 04:51:32 +0900

    Initial commit

---
+++ CoupangPartnersBlocker.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            [Newspic] CoupangPartnersBlocker
+// @name:ko         [뉴스픽] 쿠팡 파트너스 배너 제거기
+// @namespace       https://github.com/No-Eul/Userscripts
+// @version         1.0.3
+// @description     Don't go to the Coupang website on a Newspic post, just view it!
+// @description:ko  쿠팡 파트너스로 등록된 게시물을 쿠팡 사이트 접속 없이 볼 수 있게 합니다.
+// @author          NoEul
+// @license         MIT License - https://github.com/No-Eul/Userscripts/blob/CoupangPartnersBlocker/LICENSE.txt
+// @source          https://github.com/No-Eul/Userscripts/tree/CoupangPartnersBlocker
+// @supportURL      https://github.com/No-Eul/Userscripts/issues
+// @updateURL       https://github.com/No-Eul/Userscripts/raw/CoupangPartnersBlocker/CoupangPartnersBlocker.user.js
+// @downloadURL     https://github.com/No-Eul/Userscripts/raw/CoupangPartnersBlocker/CoupangPartnersBlocker.user.js
+// @match           *://m.newspic.kr/*
+// @match           *://newspic.kr/*
+// @icon            https://newspic.kr/favicon.ico
+// ==/UserScript==
+
+(() => {
+	let content = document.getElementById("bo_v_atc");
+	content.querySelectorAll("div.continue_reading, div.continue_coupang").forEach($ => $.remove());
+	content.classList.remove("omit_wrap");
+	content.style.removeProperty("max-height");
+})();
---
+++ README.md
@@ -0,0 +1,2 @@
+# CoupangPartnersBlocker
+Don't go to the Coupang website on a Newspic post, just view it!
